b92593123ac59728c73a755c5f0ab28570f5c1ee
Zhaofeng Li <hello@zhaofeng.li>
2017-06-01 23:47:39 -0700

    vim: Make skeletons public

---
+++ vim/templates/skeleton.oj.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+#ifdef ONLINE_JUDGE
+#define D(x)
+#else
+#define D(x) x
+#endif
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
+
